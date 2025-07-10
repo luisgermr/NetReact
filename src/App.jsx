2516a23653afef1599c8d9dfca9314bd3151b345
luisgermr <luisger_mr@hotmail.com>
2025-07-09 21:58:18 -0500

    Cards added

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,27 @@ function App() {
       <h1 className='text-primary'>Hola, esto es un SPA usando React</h1>
       <p className='lead'>Esto es un proyecto usando Vite y Bootstrap</p>
       <button className='btn btn-success'>Boton de test</button>
+    <hr></hr>
+      <div class="card" >
+        <div class="card-body">
+          <h5 class="card-title">Vite</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+          <p class="card-text">Vite is a blazing fast frontend build tool powering the next generation of web applications.</p>
+          <a href="#" class="card-link">Pagina Oficial</a>
+        </div>
+      </div>
+      <div class="card" >
+        <div class="card-body">
+          <h5 class="card-title">React</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+          <p class="card-text">React te permite construir interfaces de usuario a partir de piezas individuales llamadas componentes. Crea tus propios componentes de React como Thumbnail, LikeButton, y Video. Luego combínalos para formar pantallas, páginas y aplicaciones.</p>
+          <a href="#" class="card-link">Pagina Oficial</a>
+        </div>
+      </div>
+
     </div>
+
+
   )
 }
 
